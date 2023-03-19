e738586c5a39c9275093c54ec6cf91d94da57226
Krijn Hoetmer <krijn@qontent.nl>
2023-03-19 11:51:18 +0100

    Add note about duplicate URLs to docs

--- src/routes/docs/urls/+page.svelte
+++ src/routes/docs/urls/+page.svelte
@@ -24,6 +24,8 @@
 	"websiteId": "clf7ymhtc055k0bw0bufs69q3"
 &rbrace;</code></pre>
 
+<p>Hou er rekening mee dat een URL uniek moet zijn. Als je een al bestaande URL toevoegt, krijgt je een fout terug (de URL wordt dan dus niet dubbel toegevoegd).</p>
+
 <ul><li>
   <a href="/api/v1/urls">/api/v1/urls</a>
 </li></ul>
