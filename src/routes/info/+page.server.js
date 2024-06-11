edd4c459162ffea767e6580dc4c380530a04a3c8
RayanSp <rayan.spall@hva.nl>
2024-06-11 11:56:49 +0200

    Added form enhancement💻
     - Added succesmessage and errormessage
     - Made some CSS changes for the form

--- src/routes/info/+page.server.js
+++ src/routes/info/+page.server.js
@@ -4,18 +4,21 @@ import { request as graphqlRequest } from "graphql-request";
 import { gql } from "graphql-request";
 import { hygraph } from "$lib/utils/hygraph.js";
  
-export const prerender = false;
- 
-// Data naar Hygraph sturen
+
 export const actions = {
-    default: async ({ request, url }) => {
- 
+    default: async ({ request }) => {
         const formData = await request.formData();
- 
-        let name = formData.get("name");
-        let email = formData.get("email");
-        let vraag = formData.get("vraag");
-        // Maak nieuwe content aan voor Hygraph
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const vraag = formData.get("vraag");
+
+        if (!name || !email || !vraag) {
+            return fail(400, {
+                error: "Vul alle vereiste velden in.",
+                values: { name, email, vraag },
+            });
+        }
+
         const mutation = `
         mutation {
             createContact(data: { name: "${name}", email: "${email}", vraag: "${vraag}" }) {
@@ -25,24 +28,21 @@ export const actions = {
             }
         }
         `;
- 
-        // Hygraph url
-        const endpoint =
-            "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbe0zp4u2fkz01uj486xdza4/master";
- 
-        // Hygraph autorisatie
-        const HYGRAPH_TOKEN = import.meta.env.HYGRAPH_KEY;
+
+        const endpoint = "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbe0zp4u2fkz01uj486xdza4/master";
+        const HYGRAPH_TOKEN = import.meta.env.VITE_HYGRAPH_KEY;
         const headers = {
             Authorization: `Bearer ${HYGRAPH_TOKEN}`,
         };
- 
-        // Voer de mutatie uit
-        const postData = await graphqlRequest(
-            endpoint,
-            mutation,
-            undefined,
-            headers
-        );
-        return { success: true, postData };
+
+        try {
+            const postData = await graphqlRequest(endpoint, mutation, undefined, headers);
+            return { success: true, data: { message: "Verzonden!" } };
+        } catch (err) {
+            return fail(500, {
+                error: "Er is een fout opgetreden. Probeert u het alstublieft opnieuw.",
+                values: { name, email, vraag },
+            });
+        }
     },
 };
--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,100 +1,136 @@
 <script>
-	import { enhance } from '$app/forms'
-	import Heading from '$lib/components/heading.svelte';
-	import walking from '$lib/assets/walking_together.svg';
-	import knowledge from '$lib/assets/sharing_knowledge.svg';
+    import { enhance } from '$app/forms';
+    import Heading from '$lib/components/heading.svelte';
+    import walking from '$lib/assets/walking_together.svg';
+    import knowledge from '$lib/assets/sharing_knowledge.svg';
 
-    export let data 
+    export let data;
 
-	$: heading = { titel: 'Informatie' };
+    $: heading = { titel: 'Informatie' };
 
+    let isSubmitting = false;
+    let successMessage = '';
+    let errorMessage = '';
 
+    function handleEnhance({ formElement }) {
+        const handleSubmit = async ({ result }) => {
+            isSubmitting = false;
+            successMessage = '';
+            errorMessage = '';
+
+            if (result.type === 'failure') {
+                errorMessage = result.data.error;
+            } else if (result.type === 'success') {
+                formElement.reset();
+                successMessage = result.data.message;
+            }
+        };
+        return handleSubmit;
+    }
 </script>
 
 <Heading {heading} />
 
 <div class="content-container">
-	<section class="top-container">
-		<section class="text-container">
-			<h3><span>Vervoerregio Amsterdam.</span> HvA</h3>
-			<br />
-			<p>
-				Wij hebben samen met studenten van de Hogeschool van Amsterdam een checklist ontwikkeld
-				waarin op eenvoudige wijze wordt uitgelegd wat hierbij komt kijken en welke stappen u kunt
-				nemen om uw website en app toegankelijk te maken.
-			</p>
-		</section>
-		<section class="image-container">
-			<img src={walking} class="text-image" alt="darkmode icon" />
-		</section>
-	</section>
+    <section class="top-container">
+        <section class="text-container">
+            <h3><span>Vervoerregio Amsterdam.</span> HvA</h3>
+            <br />
+            <p>
+                Wij hebben samen met studenten van de Hogeschool van Amsterdam een checklist ontwikkeld
+                waarin op eenvoudige wijze wordt uitgelegd wat hierbij komt kijken en welke stappen u kunt
+                nemen om uw website en app toegankelijk te maken.
+            </p>
+        </section>
+        <section class="image-container">
+            <img src={walking} class="text-image" alt="darkmode icon" />
+        </section>
+    </section>
 
-	<section class="left-container">
-		<h3 id="wettelijk">is uw website al toegankelijk?</h3>
-		<br />
-		<p>
-			Binnen 2025 moet dit al! Voor overheden is dit al verplicht. En vanaf 2025 moet iedere nieuwe
-			website en app digitaal toegankelijk zijn. Dit moet volgens het EAA (European Accessibility
-			Act). Dit zou er mee moeten helpen dat iedereen elke website zou kunnen bezoeken, niet
-			uitmakende dat iemand een beperking heeft
-		</p>
-	</section>
+    <section class="left-container">
+        <h3 id="wettelijk">is uw website al toegankelijk?</h3>
+        <br />
+        <p>
+            Binnen 2025 moet dit al! Voor overheden is dit al verplicht. En vanaf 2025 moet iedere nieuwe
+            website en app digitaal toegankelijk zijn. Dit moet volgens het EAA (European Accessibility
+            Act). Dit zou er mee moeten helpen dat iedereen elke website zou kunnen bezoeken, niet
+            uitmakende dat iemand een beperking heeft
+        </p>
+    </section>
 
-	<section class="right-container">
-		<h3>Wat kunt u doen om uw website toegankelijk te maken?</h3>
-		<br />
-		<p>
-			De Vervoerregio Amsterdam heeft samen met studenten van de Hogeschool van Amsterdam een
-			checklist ontwikkeld waarin op eenvoudige wijze wordt uitgelegd wat hierbij komt kijken en
-			welke stappen u kunt nemen om uw website en app toegankelijk te maken.
-		</p>
-	</section>
+    <section class="right-container">
+        <h3>Wat kunt u doen om uw website toegankelijk te maken?</h3>
+        <br />
+        <p>
+            De Vervoerregio Amsterdam heeft samen met studenten van de Hogeschool van Amsterdam een
+            checklist ontwikkeld waarin op eenvoudige wijze wordt uitgelegd wat hierbij komt kijken en
+            welke stappen u kunt nemen om uw website en app toegankelijk te maken.
+        </p>
+    </section>
 
-	<section class="bot-left-container">
-		<section class="text-container">
-			<h3>Wat is de wettelijke achtergrond?</h3>
-			<br />
-			<p>
-				In Nederland zijn er ca. 2 miljoen mensen met een beperking. Als deze mensen uw website of
-				app niet kunnen gebruiken, dan zorgt dat dat deze mensen worden uitgesloten van de
-				samenleving. Medio 2025 wordt om die reden door.
-				<br /><br />
-				De Europese Unie de European Accessibility Act (EAA) of Europese Toegankelijkheidswet ingevoerd.
-				De wet zorgt ervoor dat digitale barrières worden verwijderd voor mensen met een beperking. De
-				eisen zijn gebaseerd op de Web Content Accessibility Guidelines (WCAG) van het World Wide Web
-				Consortium
-			</p>
-		</section>
-		<section class="image-container">
-			<img src={knowledge} class="text-image" alt="Sharing Knowledge" />
-		</section>
-	</section>
+    <section class="bot-left-container">
+        <section class="text-container">
+            <h3>Wat is de wettelijke achtergrond?</h3>
+            <br />
+            <p>
+                In Nederland zijn er ca. 2 miljoen mensen met een beperking. Als deze mensen uw website of
+                app niet kunnen gebruiken, dan zorgt dat dat deze mensen worden uitgesloten van de
+                samenleving. Medio 2025 wordt om die reden door.
+                <br /><br />
+                De Europese Unie de European Accessibility Act (EAA) of Europese Toegankelijkheidswet ingevoerd.
+                De wet zorgt ervoor dat digitale barrières worden verwijderd voor mensen met een beperking. De
+                eisen zijn gebaseerd op de Web Content Accessibility Guidelines (WCAG) van het World Wide Web
+                Consortium
+            </p>
+        </section>
+        <section class="image-container">
+            <img src={knowledge} class="text-image" alt="Sharing Knowledge" />
+        </section>
+    </section>
 
-	<section class="bot-right-container">
-		<section class="text-container">
-			<h3>Contact opnemen?</h3>
-			<br />
-			<p>
-				Indien u vragen heeft of mocht er nog enige onduidelijkheid bestaan, kunt u via 
-                onderstaand formulier contact met ons opnemen.
-			</p>
-            <form action="/info" method="POST">
+    <section class="bot-right-container">
+        <section class="text-container">
+            <h3>Contact opnemen?</h3>
+            <br />
+            <p>
+                Indien u vragen heeft of mocht er nog enige onduidelijkheid bestaan, kunt u via onderstaand
+                formulier contact met ons opnemen.
+            </p>
+            <form
+                action="/info"
+                use:enhance={handleEnhance}
+                on:submit={() => (isSubmitting = true)}
+                method="POST"
+            >
                 <fieldset class="form-vraag">
-                <label for="name">Naam</label>
-                <input id="name" placeholder="name" type="text" name="name" required />
-                <label for="mail">Email</label>
-                <input placeholder="email" id="mail" type="email" name="email" required />
-                <label for="vraag">Uw vraag</label>
-                <textarea id="vraag" name="vraag" required rows="3"></textarea>
-                <button class="form-button" type="submit">Submit</button>
+                    <label for="name">Naam</label>
+                    <input id="name" placeholder="name" type="text" name="name" required />
+                    <label for="mail">Email</label>
+                    <input placeholder="email" id="email" type="email" name="email" required />
+                    <label for="vraag">Uw vraag</label>
+                    <textarea id="vraag" name="vraag" placeholder="Bericht..." required rows="3" />
+                    <button type="submit" class="form-button" disabled={isSubmitting}>
+                        {#if isSubmitting}
+                            versturen...
+                        {:else}
+                            Verstuur
+                        {/if}
+                    </button>
+
+                    {#if successMessage}
+                        <p class="success-message">{successMessage}</p>
+                    {/if}
+
+                    {#if errorMessage}
+                        <p class="error-message">{errorMessage}</p>
+                    {/if}
                 </fieldset>
             </form>
-		</section>
-	</section>
+        </section>
+    </section>
 </div>
 
 <style>
-
 	.text-container p {
 		max-width: 50rem;
 	}
@@ -104,17 +140,17 @@
 		display: flex;
 		align-items: flex-start;
 		justify-content: flex-start;
-        /* height: 20rem; */
+		/* height: 20rem; */
 	}
 
 	.left-container {
 		grid-area: left;
-        /* height: 13rem; */
+		/* height: 13rem; */
 	}
 
 	.right-container {
 		grid-area: right;
-        /* height: 13rem; */
+		/* height: 13rem; */
 	}
 
 	.bot-left-container {
@@ -134,9 +170,9 @@
 	.content-container {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
-		grid-template-rows: .3fr .5fr 1fr 1fr;
+		grid-template-rows: 0.3fr 0.5fr 1fr 1fr;
 		grid-template-areas:
-			'top top' 
+			'top top'
 			'left right'
 			'botleft botright';
 		margin: 1em 1em;
@@ -171,19 +207,18 @@
 		align-self: center;
 	}
 
-    .form-vraag {
+	.form-vraag {
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
 		width: 18rem;
-        background-color: var(--c-container);
-        border: none;
+		background-color: var(--c-container);
+		border: none;
 		border-radius: 10px;
 		padding-right: 2rem;
 		padding-top: 1rem;
 		padding-bottom: 1.5rem;
-    }
-
+	}
 
 	input {
 		background-color: var(--c-container-stroke);
@@ -193,35 +228,40 @@
 		padding-left: 0.5rem;
 		color: white;
 		font-size: 16px;
-        margin-bottom: .7rem;
+		margin-bottom: 0.7rem;
 	}
 
 	input:valid {
 		border: 1px solid lightgreen;
-		transition: .3s ease-out;
+		transition: 0.3s ease-out;
+	}
+
+	textarea:valid {
+		border: 1px solid lightgreen;
+		transition: 0.2s ease-out;
 	}
 
 	/* input:invalid {
 		border: 1px solid red;
 	} */
 
-    textarea {
-        background-color: var(--c-container-stroke);
+	textarea {
+		background-color: var(--c-container-stroke);
 		border: none;
 		height: 4rem;
 		width: 15rem;
 		padding-left: 0.5rem;
 		color: white;
 		font-size: 16px;
-    }
+	}
 
 	form:valid .form-button {
 		background-color: var(--c-pink);
 		border: 2px solid var(--c-pink);
 	}
 
-    .form-button {
-        margin-top: 1rem;
+	.form-button {
+		margin-top: 1rem;
 		margin-bottom: 1rem;
 		background-color: grey;
 		color: lightgray;
@@ -231,11 +271,18 @@
 		border-radius: 20px;
 		cursor: pointer;
 		transition: 0.2s ease-in-out;
-		font-size: 16px; 
-    }
+		font-size: 16px;
+	}
 
-    .form-button:hover {
-        background-color: transparent !important;
+	.form-button:hover {
+		background-color: transparent !important;
+	}
+
+	.success-message {
+        color: green;
+    }
+    .error-message {
+        color: red;
     }
 
 	@media only screen and (max-width: 1100px) {
@@ -247,7 +294,7 @@
 				'left'
 				'right'
 				'botleft'
-                'botright';
+				'botright';
 		}
 
 		.text-image {
