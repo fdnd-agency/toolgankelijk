1d74f0ed48c2229255905dd22c3ba98fe28cb4f1
Bjarne Zeeman <bjarne.zeeman@hva.nl>
2025-02-14 12:05:08 +0100

    test: Dummy tests toegevoegd zodat npm run test geen error geeft voor files zonder tests #86

--- tests/routes/layout.svelte.test.js
+++ tests/routes/layout.svelte.test.js
@@ -2,4 +2,7 @@ import { describe, it, expect } from 'vitest';
 
 describe('src/routes/+layout.svelte', () => {
     // Write your tests for src/routes/+layout.svelte here
+    it('adds 1 + 2 to equal 3', () => {
+		expect(1 + 2).toBe(3);
+	});
 });
