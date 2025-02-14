4a44a7bc53c9976b0bf9a98ddd22602da40ed738
Mossati <xmossati9786@gmail.com>
2025-02-14 10:09:44 +0100

    flex-box added to criteria title and button

--- src/lib/components/checklist.svelte
+++ src/lib/components/checklist.svelte
@@ -145,12 +145,14 @@
 						{#if succescriterium.niveau === selectedNiveau}
 							<details>
 								<summary class="criteria-uitklapbaar">
-										<div class="titels">
-											<span>Criteria {succescriterium.index} ({succescriterium.niveau})</span>
+									<span>Criteria {succescriterium.index} ({succescriterium.niveau})</span>
+									<div class="row">
+									<div class="column">
 											<h3>{succescriterium.titel}</h3>
-										</div>
+									</div>
 
-										<button 
+									<div class="column">
+										<button
                                             type="button" 
                                             class="btn-vertaling" 
                                             on:click={(event) => translate(event, succescriterium.index)}
@@ -164,6 +166,8 @@
 											type="checkbox"
 											checked={checkedSuccescriteria.find((e) => e.id === succescriterium.id)}
 										/>
+									</div>
+								</div>
 								</summary>
 								<!-- tekuitleg voor succescriterium -->
 
@@ -387,6 +391,20 @@
 		/* display: flex; */
 		flex-direction: row;
 		align-items: center;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 1rem;
 	}
 
 	/* .criteria-uitklapbaar::-webkit-details-marker {
