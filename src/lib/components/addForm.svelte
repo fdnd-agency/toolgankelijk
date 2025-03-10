b72b54b186c823fc8f9f84521457f5af21bef7c1
Mossati <xmossati9786@gmail.com>
2025-03-10 10:15:41 +0100

    feat: CSS order changed and small styling edits

--- src/lib/components/addForm.svelte
+++ src/lib/components/addForm.svelte
@@ -53,13 +53,13 @@
 			<input id="url" name="url" required type="url" placeholder="type een url link..."/>
 
 			{#if isUrl}
-			<label for="url" class="url-label">Slug</label>
+			<label for="slug" class="slug-label">Slug</label>
 			<input id="slug" name="slug" type="name" value={params} readonly/>
 			{/if}
 
 			<div class="button-div">
 				<button type="submit" class="add-button">Toevoegen</button>
-				<button class="remove-button" on:click={close}>Sluit</button>
+				<button class="remove-button" on:click={close}>Sluiten</button>
 			</div>
 		</form>
 	</section>
@@ -74,7 +74,7 @@
 		transform: translate(-50%, -50%);
 		overflow: visible;
 		width: 100%;
-		max-width: 30em;
+		max-width: 30rem;
 		border: none;
 		display: none;
 	}
@@ -86,6 +86,26 @@
 	dialog::backdrop {
 		background-color: rgb(44, 44, 44);
 		opacity: 0.8;
+	}
+
+	.form-container {
+		background-color: var(--c-container);
+		border: solid 0.1rem var(--c-container-stroke);
+		border-radius: 0.5rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+		gap: 1rem;
+		padding: 1.2rem;
+		width: 100%;
+	}
+
+	h2 {
+		border-bottom: 0.1rem solid var(--c-container-stroke);
+		width: 100%;
+		padding-bottom: 0.25rem;
+		color: var(--c-white);
 	}
 
 	.tip-message {
@@ -117,93 +137,6 @@
 		margin-left: 0.5rem;
 	}
 
-	.form-container {
-		background-color: var(--c-container);
-		border: solid 1px var(--c-container-stroke);
-		border-radius: 0.5em;
-		display: flex;
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: flex-start;
-		gap: 0.5rem;
-		padding: 1.2em;
-	}
-
-	.form-container {
-		line-height: 2em;
-		width: 100%;
-	}
-
-	h2 {
-		border-bottom: 1px solid var(--c-container-stroke);
-		width: 100%;
-		padding-bottom: 0.25em;
-		color: var(--c-white);
-	}
-
-	form {
-		margin-top: 1em;
-	}
-
-	.button-div {
-		display: flex;
-		flex-direction: row;
-	}
-
-	.add-button {
-		border: none;
-		background-color: var(--c-pink);
-		color: white;
-		padding: 0.5em 1em;
-		cursor: pointer;
-		text-decoration: none;
-		transition: 0.3s;
-		border-radius: 0.25em;
-		margin-top: 1em;
-		margin-right: 1rem;
-		font-size: 1em;
-	}
-
-	.remove-button {
-		border: none;
-		background-color: var(--c-container-stroke);
-		color: white;
-		padding: 0.5em 1em;
-		cursor: pointer;
-		text-decoration: none;
-		transition: 0.3s;
-		border-radius: 0.25em;
-		margin-top: 1em;
-		font-size: 1em;
-		transition: 0.2s ease-in;
-	}
-
-	.remove-button:hover {
-		background-color: var(--c-pink);
-	}
-
-	input {
-		width: 100%;
-		padding: 12px 20px;
-		display: inline-block;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		box-sizing: border-box;
-		max-width: 700px;
-	}
-
-	input:focus {
-		outline: 0.1rem solid var(--c-orange);
-	}
-
-	.add-button:hover {
-		opacity: 0.75;
-	}
-
-	.url-label {
-		margin-top: 1em;
-	}
-
 	form {
 		width: 100%;
 		display: flex;
@@ -214,5 +147,60 @@
 
 	label {
 		color: var(--c-white);
+		margin-bottom: 0.5rem;
+	}
+
+	input {
+		width: 100%;
+		padding: 0.5rem 1rem;
+		display: inline-block;
+		border: 0.1rem solid var(--c-white);
+		border-radius: 0.25rem;
+		max-width: 30rem;
+		margin-bottom: 1rem;
+		font-size: 1rem;
+	}
+
+	input:focus {
+		outline: 0.1rem solid var(--c-orange);
+	}
+
+	input:read-only {
+		background-color: grey;
+		color: var(--c-white);
+	}
+
+	.button-div {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
+		width: 100%;
+	}
+
+	button {
+		font-size: 1rem;
+		border: none;
+		color: var(--c-white);
+		padding: 0.5rem 1rem;
+		cursor: pointer;
+		transition: 0.2s ease;
+		border-radius: 0.25rem;
+		width: 50%;
+	}
+
+	.add-button {
+		background-color: var(--c-pink);
+	}
+
+	.remove-button {
+		background-color: var(--c-container-stroke);
+	}
+
+	.add-button:hover {
+		opacity: 0.75;
+	}
+
+	.remove-button:hover {
+		background-color: var(--c-pink);
 	}
 </style>
