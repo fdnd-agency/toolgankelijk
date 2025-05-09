d1b8aadb9b941dda4d09f891feb741ba41ffb883
Mossati <xmossati9786@gmail.com>
2025-05-09 12:24:29 +0200

    feat: fixed post handling

--- src/lib/components/addForm.svelte
+++ src/lib/components/addForm.svelte
@@ -111,7 +111,8 @@
 			</div>
 
 			{#if sending}
-				<div>
+				<div class="loader-container" aria-label="loading message">
+					<span class="loader"></span>
 					<p>Partner wordt toegevoegd...</p>
 				</div>
 			{/if}
@@ -311,5 +312,46 @@
 
 	.remove-button:hover {
 		opacity: 0.75;
+	}
+
+	.loader-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+		width: 100%;
+		padding: 1rem 0;
+		background-color: var(--c-container-stroke);
+		border-radius: 0.25rem;
+		margin-top: 1rem;
+		color: var(--c-white);
+	}
+
+	.loader {
+        width: 48px;
+        height: 48px;
+		border-radius: 50%;
+		position: relative;
+		transform:rotate(45deg);
+		background: #fff;
+	}
+		
+	.loader::before {
+		content: "";
+		box-sizing: border-box;
+		position: absolute;
+		inset: 0px;
+		border-radius: 50%;
+		border:24px solid var(--c-pink);
+		animation: prixClipFix 2s infinite linear;
+	}
+
+	@keyframes prixClipFix {
+		0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+		25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+		50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+		75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+		100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
 	}
 </style>
