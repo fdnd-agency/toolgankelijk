fe1fc9ddaf7087715ecdbbaedae2d64f5a6536d0
Mossati <xmossati9786@gmail.com>
2025-06-02 11:45:19 +0200

    feat: added new svg icons

--- src/lib/components/partner.svelte
+++ src/lib/components/partner.svelte
@@ -134,16 +134,16 @@
 			</div>
 			<div class="icons" id={`icons-${website.id}`}>
 				{#if !isUrl}
-					<button on:click={openForm.bind(null, auditType)}>
-						<img width="24" height="24" src={pencil} alt="Audit icon" />
+					<button on:click={openForm.bind(null, auditType)} aria-label="start audit button">
+						<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-list-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3.5 5.5l1.5 1.5l2.5 -2.5" /><path d="M3.5 11.5l1.5 1.5l2.5 -2.5" /><path d="M3.5 17.5l1.5 1.5l2.5 -2.5" /><path d="M11 6l9 0" /><path d="M11 12l9 0" /><path d="M11 18l9 0" /></svg>
 					</button>
 				{/if}
-				<button on:click={openForm.bind(null, editType)}
-					><img width="24" height="24" src={pencil} alt="Bewerk icon" /></button
-				>
-				<button on:click={openForm.bind(null, deleteType)}
-					><img width="24" height="24" src={trash} alt="Verwijder icon" /></button
-				>
+				<button on:click={openForm.bind(null, editType)} aria-label="edit button">
+					<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
+				</button>
+				<button on:click={openForm.bind(null, deleteType)} aria-label="delete button">
+					<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
+				</button>
 			</div>
 		</section>
 
@@ -327,6 +327,11 @@
 
 	.container-off {
 		display: none;
+	}
+
+	svg {
+		fill: transparent;
+		stroke: white;
 	}
 
 	@media (inverted-colors: inverted) {
