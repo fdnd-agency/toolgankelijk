097f571c0021714707da921f27909060edd7cd0b
Bjarne Zeeman <bjarne.zeeman@hva.nl>
2025-06-12 14:47:35 +0200

    fix: Create en Update sessie mutaties maken gebruik van het zelfde type voor verval datum

--- src/lib/server/session.js
+++ src/lib/server/session.js
@@ -60,7 +60,7 @@ export async function validateSessionToken(token) {
 		sessie.houdbaarTot = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
 		// Update session mutation
 		const updateMutation = gql`
-			mutation UpdateSessie($id: ID!, $expiresAt: Float!) {
+			mutation UpdateSessie($id: ID!, $expiresAt: Date!) {
 				updateSessie(where: { sessieId: $id }, data: { houdbaarTot: $expiresAt }) {
 					id
 				}
