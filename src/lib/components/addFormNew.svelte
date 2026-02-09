93b232ee630a84a88d545c30f898c7f47044309d
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-02-09 14:38:35 +0100

    build: code
    #184

---
+++ src/lib/components/addFormNew.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+<dialog>
+    <section class="color-primary-light">
+        <div class="add-form-heading">
+            <h2>Voeg $naam toe</h2>
+            <button class="add-form-cross">
+                <img src="./src/lib/assets/svg/cross-icon" width="32">
+            </button>
+        </div>
+
+        <div>
+            <!-- exclmation mark -->
+            <img src="./src/lib/assets/svg/exclamation-icon" width="32">
+            <p> Tip </p>
+        </div>
+
+            <form>
+
+                <input type="text" placeholder="type in title">
+
+                <input type="text" placeholder="type in url">
+
+                <input type="checkbox" id="sitemap" name="Include sitemap">
+
+                <button>Partner toevoegen</button>
+            </form>
+
+
+
+    </section>
+</dialog>
+
+<style>
+    .add-form-heading {
+        margin: 1em;
+        background-color: var(--color);
+    }
+</style>
