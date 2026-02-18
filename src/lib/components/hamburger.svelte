49467354e7fc28bcaa20e64b55b5c727d8f4a82d
RickFDND <rick.pijnacker@hva.nl>
2026-02-18 14:32:32 +0100

    style: some hamburger menu styling #176  #180

--- src/lib/components/hamburger.svelte
+++ src/lib/components/hamburger.svelte
@@ -1,7 +1,32 @@
-<a href="#footer" aria-label="open menu">
+<a href="#footer" aria-label="open menu" class="color-primary">
+    <p>Menu</p>
     <svg width="29" height="18" viewBox="0 0 29 18" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
         <rect y="7.875" width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
         <rect y="15.75" width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
     </svg>
 </a>
+
+
+<style>
+    a {
+        display: flex;
+        gap: 1em;
+        align-items: center;
+        height: 35px;
+        width: auto;
+        grid-column: 3;
+        margin-left: auto;
+        text-decoration: none;
+        color: #B9005F;
+
+        p {
+            font-weight: 700;
+        }
+
+        svg {
+            height: 2.5rem;
+            width: 2.5rem;
+        }
+    }
+</style>
