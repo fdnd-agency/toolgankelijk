f25fbea1378ff9a41d8120ab38f8d3076f2df5ca
RickFDND <rick.pijnacker@hva.nl>
2026-02-20 09:05:52 +0100

    refactor: small refactor #198

--- src/lib/components/headernew.svelte
+++ src/lib/components/headernew.svelte
@@ -137,7 +137,7 @@
         display: flex;
         flex-direction: column;
         position: relative;
-        padding: 1rem 0 1rem 0;
+        padding: 1rem 0;
 		overflow-x: clip;
 
         .header-wrapper {
