f3ee527d18c9e259bf62edc6f98175511a646e8c
RickFDND <rick.pijnacker@hva.nl>
2026-03-05 12:01:00 +0100

    feat: made signout button a component #180  #198

---
+++ src/lib/components/signoutButton.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<button onclick={handleSignOut} class="signout-btn">Sign out</button>
+
+<style>
+    .signout-btn {
+		background-color: #b5006c;
+		color: var(--c-white);
+		border: 2px solid #b5006c;
+		border-radius: 20px;
+		width: 8rem;
+		height: 2.2rem;
+		font-size: 1rem;
+		font-weight: 600;
+		cursor: pointer;
+		transition:
+			background 0.2s,
+			color 0.2s,
+			border 0.2s;
+	}
+
+	.signout-btn:hover {
+		background-color: transparent;
+		color: #b5006c;
+		border: 2px solid #b5006c;
+	}
+</style>
