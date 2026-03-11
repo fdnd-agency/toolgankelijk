6d9a431fe6c5ea76245848467446902c76c098b8
RickFDND <rick.pijnacker@hva.nl>
2026-03-11 13:22:54 +0100

    style: style hamburgerbutton for opening menu #198

--- src/lib/components/hamburger.svelte
+++ src/lib/components/hamburger.svelte
@@ -1,39 +1,31 @@
-<a href="#footer" aria-label="open menu" class="color-primary">
+<a href="#main-nav" aria-label="open menu" class="hamburger-link">
     <p>Menu</p>
     <svg width="29" height="18" viewBox="0 0 29 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
-        <rect y="7.875" width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
-        <rect y="15.75" width="28.125" height="2.25" rx="1.125" fill="#B9005F"/>
+        <rect width="28.125" height="2.25" rx="1.125" fill="currentColor"/>
+        <rect y="7.875" width="28.125" height="2.25" rx="1.125" fill="currentColor"/>
+        <rect y="15.75" width="28.125" height="2.25" rx="1.125" fill="currentColor"/>
     </svg>
 </a>
 
 <style>
-    a {
+    .hamburger-link {
         display: flex;
-        gap: 1em;
+        gap: 0.5em;
         align-items: center;
-        height: 35px;
-        width: auto;
-        grid-column: 3;
-        margin-left: auto;
         text-decoration: none;
         color: var(--color-primary);
+        z-index: 20;
+        grid-column: 3;
+        margin-left: auto;
 
         p {
             font-weight: 700;
         }
-
-        svg {
-            height: 2.5rem;
-            width: 2.5rem;
-
-            rect {
-                fill: var(--color-primary);
-            }
-        }
-
-        @media (min-width: 1260px) {
-            display: none;
+    }
+    
+    @media (min-width: 1260px) {
+        .hamburger-link { 
+            display: none; 
         }
     }
 </style>
