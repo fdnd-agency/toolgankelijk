6e112a4baaa63d0379765ce13af8f648c5ecf0b3
Maksim <maksim.hofker@hva.nl>
2026-03-17 11:53:04 +0100

    feat: added getQueryLayout query to fetch layout data

---
+++ src/lib/queries/layout.js
@@ -0,0 +1,74 @@
+export default function getQueryLayout(gql) {
+	return gql`
+		query Layout(
+			$slug: String
+			$partnerLimit: Int!
+			$partnerOffset: Int!
+			$urlLimit: Int!
+			$urlOffset: Int!
+		) {
+			partnerOverview: toolgankelijk_website(limit: $partnerLimit, offset: $partnerOffset) {
+				id
+				title
+				slug
+				homepage
+				urls {
+					id
+					slug
+					url
+					checks {
+						id
+						successCriteria: success_criteria {
+							id
+						}
+					}
+				}
+			}
+
+			partnerOverview_agg: toolgankelijk_website_aggregated {
+				count {
+					id
+				}
+			}
+
+			website: toolgankelijk_website(filter: { slug: { _eq: $slug } }, limit: 1) {
+				id
+				title
+				slug
+				homepage
+				urls(limit: $urlLimit, offset: $urlOffset) {
+					id
+					url
+					name
+					slug
+					checks {
+						id
+						successcriteria: success_criteria {
+							id
+						}
+					}
+				}
+			}
+
+			websiteUrlAgg: toolgankelijk_url_aggregated(
+				filter: { website_id: { slug: { _eq: $slug } } }
+			) {
+				count {
+					id
+				}
+			}
+
+			principes: toolgankelijk_principle {
+				id
+				title
+				guidelines: Guidelines {
+					id
+					toolgankelijk_guideline_id {
+						id
+					}
+				}
+			}
+		}
+	`;
+}
+
