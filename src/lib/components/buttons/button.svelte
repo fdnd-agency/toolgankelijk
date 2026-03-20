6acb4e1eb74a745d4d601fd541d0265dd21b2382
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-20 13:03:21 +0100

    build: sizes
    all the button has size classes
    
    #198

--- src/lib/components/buttons/button.svelte
+++ src/lib/components/buttons/button.svelte
@@ -1,21 +1,39 @@
 <script>
-	let { icon, title, onclick } = $props();
+	let { icon, title, onclick, imgsrc, size } = $props();
 </script>
 
-<button onclick={onclick}>
+<button onclick={onclick} class={size}>
     <!-- with svelte:component you can import component within a component -->
     <svelte:component this={icon} />
+        <img src={imgsrc} />
     <p> {title} </p>
 </button>
 
 <style>
+    .small {
+        width: 3em;
+        justify-content: center;
+
+        svg {
+            display: flex;
+            justify-content: center;
+        }
+    }
+
+    .medium {
+        width: 6em;
+    }
+
+    .large {
+        width: 12em;
+    }
+
     button {
         display: flex;
         align-items: center;
         padding-left: 1em;
         padding-right: 1em;
         border-radius: 1em;
-        gap: 1em;
         height: 3em;
         background-color: var(--color-primary-light);
     }
--- src/lib/components/buttons/navigation.svelte
+++ src/lib/components/buttons/navigation.svelte
@@ -1,27 +1,47 @@
 <script>
-	let { icon, title, href } = $props();
+	let { icon, title, href, active, size } = $props();
 </script>
 
-<a href={href}>
+<a href={href} id={active} class={size} >
     <!-- with svelte:component you can import component within a component -->
     <svelte:component this={icon} />
-    <p> {title} </p>
+    {title}
 </a>
 
 <style>
-    a {
+    .small {
+        width: 3em;
         display: flex;
         justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        width: 12em;
+    }
+
+    .medium {
+        width: 6em;
         padding-left: 1em;
         padding-right: 1em;
+    }
+
+    .large {
+        width: 9em;
+        gap: 1em;
+        padding-left: 1em;
+        padding-right: 1em;
+    }
+    
+    #active {
+        background-color: var(--color-neutral-white);
+        border: var(--border-darkgrey);
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
         border-radius: 1em;
         gap: 1em;
         height: 3em;
         background-color: var(--color-primary-light);
-        color: var(--color-neutral-dark-grey)
+        color: var(--color-neutral-dark-grey);
         border: var(--border-darkgrey);
     }
 </style>
