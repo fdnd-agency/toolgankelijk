fa1a6f2cfd7c6a164917472c9eaf9a6050473cf8
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-30 11:34:07 +0200

    build: button
    the navbutton component will be used on serveral places in the design.
    
    #183

---
+++ src/lib/components/NavButton.svelte
@@ -0,0 +1,44 @@
+<script>
+    let {
+        type,
+        variant = "primary",
+        size = "medium",
+        href,
+        as,
+        childeren,
+        ...resizeTo
+        } = $props();
+
+        const tagname = $derived(as || (href ? "a" : "button"))
+</script>
+
+<svelte:element
+    this={tagname}
+    {href}
+    {type}
+    class="btn {variant} {size}"
+    {...rest}>
+    {@render childeren?.()}
+</svelte:element>
+
+<style>
+
+/* sizes */
+.small {
+    padding: 1em 2em;
+    font-size: 16px;
+}
+
+/* medium */
+.medium {
+    padding: 1em 4em;
+    font-size: 16px;
+}
+
+.large {
+    padding: 1em 8em;
+    font-size: 16px;
+}
+
+    
+</style>
