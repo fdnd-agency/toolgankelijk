da467213506e13e7fda2c7e78d7699e7e82ead48
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-31 15:20:46 +0200

    build: account page
    seperate account page for the website
    
    #180

---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { enhance } from '$app/forms';
+	import walking from '$lib/assets/walking_together.svg';
+	import knowledge from '$lib/assets/sharing_knowledge.svg';
+	import NavButton from '$lib/components/NavButton.svelte';
+
+	let successMessage = $state('');
+	let errorMessage = $state('');
+
+	function handleEnhance({ formElement }) {
+		const handleSubmit = async ({ result }) => {
+			isSubmitting = false;
+			successMessage = 'Verzonden!';
+
+			if (result.type === 'failure') {
+				errorMessage = 'Er is iets fout gegaan';
+				errorMessage = result.data.data.error;
+			} else {
+				formElement.reset();
+				successMessage = result.data.data.message;
+			}
+		};
+		return handleSubmit;
+	}
+</script>
+
+<h1>Your Account</h1>
+
