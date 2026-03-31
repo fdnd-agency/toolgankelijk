f8044aa853897858a948285fdc93c7c6f15249ee
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-31 15:20:23 +0200

    fix: lightmode
    removed the lightmode function
    
    #180

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,10 @@
 <script>
-	import { onMount } from 'svelte';
-	import logoDarkMode from '$lib/assets/logoDarkMode.svg';
-	import logoLightMode from '$lib/assets/logoLightMode.svg';
+	import { page } from '$app/stores';
+
+	import Logo from './logoHeader.svelte';
 	import BreadCrumbs from '$lib/components/breadCrumbs.svelte';
 	import Hamburger from '$lib/components/hamburger.svelte';
+	import NavButton from './NavButton.svelte';
 
 	/**
 	 * @typedef {Object} Props
@@ -17,64 +18,37 @@
 	/** @type {Props} */
 	let { params, partners, websites, principes, user = null } = $props();
 
-	let isLightMode = $state(false);
-
-	function toggleLightMode() {
-		const savedLightMode = localStorage.getItem('lightMode');
-
-		if (savedLightMode === null || savedLightMode === 'false') {
-			isLightMode = true;
-			document.body.classList.add('lightmode');
-			localStorage.setItem('lightMode', `${isLightMode}`);
-		} else {
-			isLightMode = false;
-			document.body.classList.remove('lightmode');
-			localStorage.setItem('lightMode', `${isLightMode}`);
-		}
-	}
-
 	async function handleSignOut(event) {
 		event.preventDefault();
 		await fetch('/logout', { method: 'POST' });
 		window.location.href = '/login';
 	}
-
-	onMount(() => {
-		const savedLightMode = localStorage.getItem('lightMode');
-
-		if (savedLightMode === 'true') {
-			isLightMode = true;
-			document.body.classList.add('lightmode');
-		} else {
-			isLightMode = false;
-			document.body.classList.remove('lightmode');
-		}
-
-		const logoImage = document.getElementById('logoImage');
-		logoImage.src = isLightMode ? logoLightMode : logoDarkMode;
-
-		const icon = document.querySelector('.disable-js');
-		icon?.classList.toggle('disable-js');
-	});
 </script>
 
 <header>
 	<a href="#main" class="visible-hidden">Jump directly to main content</a>
-	<nav>
+	<Logo />
+	<nav id="header-navigation" class="color-primary-light">
+		<NavButton href="/home" size="large" showIcon={true} iconName="home" variant="primary">
+			<p>Home</p>
+		</NavButton>
+		<NavButton hreg="/info" size="large" showIcon={true} iconName="info" variant="primary">
+			<p>Info</p>
+		</NavButton>
+		<NavButton hreg="/info" size="large" showIcon={true} iconName="account" variant="primary">
+			<p>Account</p>
+		</NavButton>
+	</nav>
+</header>
+
+	<!-- <nav>
 		<a href="/" aria-label="Ga naar het overzicht met alle partners">
-			<img
-				src={isLightMode ? logoLightMode : logoDarkMode}
-				class="vvr-logo"
-				id="logoImage"
-				alt="Logo van Vervoerregio Amsterdam"
-			/>
-		</a>
 		{#if user && user.isEmailGeverifieerd}
 			<BreadCrumbs {params} {partners} {websites} {principes} />
 		{/if}
 		<div class="options">
 			{#if user && user.isEmailGeverifieerd}
-				<a aria-label="home pagina" href="/">
+				<!-- <a aria-label="home pagina" href="/">
 					<svg
 						class="home-icon-img"
 						alt="back to homepage"
@@ -87,25 +61,8 @@
 							id="my-svg4"
 						/>
 					</svg>
-				</a>
-			{/if}
-			<a aria-label="informatie pagina" href="/info">
-				<svg
-					class="information-icon-img"
-					alt="information icon"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						id="my-svg3"
-						d="M10.8 8.4H13.2V6H10.8M12 21.6C6.708 21.6 2.4 17.292 2.4 12C2.4 6.708 6.708 2.4 12 2.4C17.292 2.4 21.6 6.708 21.6 12C21.6 17.292 17.292 21.6 12 21.6ZM12 0C10.4241 0 8.86371 0.310389 7.4078 0.913446C5.95189 1.5165 4.62902 2.40042 3.51472 3.51472C1.26428 5.76516 0 8.8174 0 12C0 15.1826 1.26428 18.2348 3.51472 20.4853C4.62902 21.5996 5.95189 22.4835 7.4078 23.0866C8.86371 23.6896 10.4241 24 12 24C15.1826 24 18.2348 22.7357 20.4853 20.4853C22.7357 18.2348 24 15.1826 24 12C24 10.4241 23.6896 8.86371 23.0866 7.4078C22.4835 5.95189 21.5996 4.62902 20.4853 3.51472C19.371 2.40042 18.0481 1.5165 16.5922 0.913446C15.1363 0.310389 13.5759 0 12 0ZM10.8 18H13.2V10.8H10.8V18Z"
-					/>
-				</svg>
-			</a>
-			{#if !user}
-				<a href="/login" aria-label="account">
+				</a> -->
+				<!-- <a href="/login" aria-label="account">
 					<svg
 						class="account-icon-img"
 						alt="account icon"
@@ -123,15 +80,15 @@
 							d="M1.5,23.48l.37-2.05A10.3,10.3,0,0,1,12,13h0a10.3,10.3,0,0,1,10.13,8.45l.37,2.05"
 						/>
 					</svg>
-				</a>
+				</a> -->
 			{/if}
 			{#if user}
 				<!-- signout button -->
 			{/if}
 		</div>
 		<Hamburger />
-	</nav>
-</header>
+	</nav> -->
+
 
 <style>
 
