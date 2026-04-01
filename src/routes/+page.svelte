38df680adabf6365a5eefd52973b78e3941aa848
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-04-01 12:57:29 +0200

    idem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import Dialog from '$lib/components/dialog.svelte';
 	import Pages from '$lib/components/pages.svelte';
 	import NavButton from '$lib/components/NavButton.svelte';
+	import Heading from '$lib/components/heading.svelte';
 
 	let { data, form } = $props();
 
@@ -35,6 +36,8 @@
 		}
 	});
 </script>
+
+<Heading {heading}/>
 
 <section>
 	<NavButton
