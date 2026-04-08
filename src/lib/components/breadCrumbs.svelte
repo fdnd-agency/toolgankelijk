dd83522a2d92e687d6809987b3cc3d004456a4fd
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-04-08 13:55:16 +0200

    build: dropdown-list
    the dropdown list defines every dropdown of the breadcrumbs
    
    #180

--- src/lib/components/breadCrumbs.svelte
+++ src/lib/components/breadCrumbs.svelte
@@ -20,122 +20,84 @@ function toggleDropdown() {
 </script>
 
 <div class="breadcrumbs">
+	<div class="breadcrumb-item">
+    <NavButton onclick={() => toggleDropdown('partner')} size="large" aria="breadcrumb of {selectedPartner}" showIcon={true} iconName="arrow" effect="reverse">
+        {#if selectedPartner}
+            <img width="24" src="{faviconAPI}{selectedPartner.homepage}/&size=128" alt="logo partner" />
+            <p>{selectedPartner.titel}</p>
+        {:else}
+            <p>Partners overzicht</p>
+        {/if}
+    </NavButton>
 
-		<NavButton onclick={toggleDropdown} size="large" aria="breadcrumb of {selectedPartner}" showIcon={true} iconName="arrow" effect="reverse">
-				{#if selectedPartner}
-					<img width="24" src="{faviconAPI}{selectedPartner.homepage}/&size=128" alt="logo partner" />
-					<p> {selectedPartner.titel} </p>
-				{:else}
-					<p>Partners overzicht</p>
-				{/if}
+    {#if activeDropdown === 'partner'}
+        <ul class="dropdown-list" transition:slide={{ duration: 200 }}>
+            {#each partners.websites as partner}
+                {#if partner}
+                    <li>
+                        <NavButton variant="primary" href="/{partner.slug}" effect="select">
+                            <img width="24" src="{faviconAPI}{partner.homepage}/&size=256" alt="logo partner"/>
+                            <p>{partner.titel}</p>
+                        </NavButton>
+                    </li>
+                {/if}
+            {/each}
+        </ul>
+    {/if}
+	</div>
 
-		</NavButton>
+    {#if websites}
+	<div class="breadcrumb-item">
+        <NavButton onclick={() => toggleDropdown('url')} size="large" aria="breadcrumb of {selectedUrl}" showIcon={true} iconName="arrow" effect="reverse">
+            {#if selectedUrl} <p>{selectedUrl}</p>
+            {:else}
+                <p>URL overzicht</p>
+            {/if}
+        </NavButton>
 
-		<ul class="dropdown-url" class:open={isOpen}>
-				{#each partners.websites as partner}
-					{#if partner}
-						<li>
-							<NavButton variant="primary" href="/{partner.slug}" effect="select">
-									<img
-										width="24"
-										src="{faviconAPI}{partner.homepage}/&size=256"
-										alt="logo partner"/>
-									<p>
-										{partner.titel}
-									</p>
+        {#if activeDropdown === 'url'}
+            <ul class="dropdown-list" transition:slide={{ duration: 200 }}>
+                {#each websites.urls as website}
+                    {#if selectedPartner && website && website.slug}
+                        <li>
+                            <NavButton href="/{selectedPartner.slug}/{website.slug}" effect="full">
+                                <p>{website.slug}</p>
+                            </NavButton>
+                        </li>
+                    {/if}
+                {/each}
+            </ul>
+        {/if}
+	</div>
+    {/if}
+
+
+    {#if selectedUrl && principes}
+		<div class="breadcrumb-item">
+        <NavButton onclick={() => toggleDropdown('principe')} size="medium" variant="primary" effect="reverse">
+            {#if selectedPrincipe}
+                <span>{selectedPrincipe.titel}</span>
+            {:else}
+                <span>Principes overzicht</span>
+            {/if}
+        </NavButton>
+
+        {#if activeDropdown === 'principe'}
+            <ul class="dropdown-list" transition:slide={{ duration: 200 }}>
+                {#each principes as principe}
+                    {#if selectedPartner && selectedUrl && principe && principe.slug}
+                        <li>
+						<NavButton href="/{selectedPartner.slug}/{selectedUrl}/{principe.slug}" effect="full"> 
+                                <p>{principe.titel}</p>
 							</NavButton>
-						</li>
-					{/if}
-				{/each}
-		</ul>
-
-		<NavButton onclick={toggleDropdown} size="large" aria="breadcrumb of {selectedPartner}" showIcon={true} iconName="arrow" effect="reverse">
-				{#if selectedPartner}
-					<img width="24" src="{faviconAPI}{selectedPartner.homepage}/&size=128" alt="logo partner" />
-					<p> {selectedPartner.titel} </p>
-				{:else}
-					<p>Partners overzicht</p>
-				{/if}
-		</NavButton>
-
-		<ul class="dropdown-partners">
-				{#each websites.urls as website}
-					{#if selectedPartner && website && website.slug}
-						<li>
-							<NavButton href="/{selectedPartner.slug}/{website.slug}" effect="reverse">
-								<p>
-									{website.slug}
-								</p>
-							</NavButton>
-						</li>
-					{/if}
-				{/each}
-			</ul>
+                        </li>
+                    {/if}
+                {/each}
+            </ul>
+        {/if}
+		</div>
+    {/if}
 </div>
-
-<!-- <div class="bread-crumbs">
-	{#if websites}
-		<span class="seperator">/</span>
-		<div class="dropdown">
-			<NavButton size="large" aria="breadcrumb of {selectedUrl}" width="full">
-				{#if selectedUrl}
-					<p>{selectedUrl}</p>
-				{:else}
-					<p>Urls overzicht</p>
-				{/if}
-			</NavButton>
-			<ul>
-				<li>
-					{#if selectedPartner}
-					<NavButton size="large" aria="breadcrumb of {selectedPartner.slug}" width="full">
-						<p>URLs overzicht</p>
-					</NavButton>
-					{:else}
-						<span>Urls overzicht</span>
-					{/if}
-				</li>
-				{#each websites.urls as website}
-					{#if selectedPartner && website && website.slug}
-						<li>
-							<a href="/{selectedPartner.slug}/{website.slug}"><span>{website.slug}</span></a>
-						</li>
-					{/if}
-				{/each}
-			</ul>
-		</div>
-	{/if}
-
-	{#if selectedUrl && principes}
-		<span class="seperator">/</span>
-		<div class="dropdown">
-			<NavButton size="medium" variant="secondary">
-				{#if selectedPrincipe}
-					<span>{selectedPrincipe.titel}</span>
-				{:else}
-					<span>Principes overzicht</span>
-				{/if}
-			</NavButton>
-			<ul>
-				<li>
-					{#if selectedPartner}
-						<a href="/{selectedPartner.slug}/{selectedUrl}"><span>Principes overzicht</span></a>
-					{:else}
-						<span>Principes overzicht</span>
-					{/if}
-				</li>
-				{#each principes as principe}
-					{#if selectedPartner && selectedUrl && principe && principe.slug}
-						<li>
-							<a href="/{selectedPartner.slug}/{selectedUrl}/{principe.slug}">
-								<span>{principe.titel}</span>
-							</a>
-						</li>
-					{/if}
-				{/each}
-			</ul>
-		</div>
-	{/if}
-</div> -->
 
 <style>
 	.breadcrumbs {
