ff3305772177ddd4f1022eee9aa5e39478704041
MaksimH2O <93390095+MaksimH2O@users.noreply.github.com>
2026-04-13 16:07:40 +0200

    refactor: updated session function calls to use new sessionService class functions

--- src/routes/logout/+server.js
+++ src/routes/logout/+server.js
@@ -1,14 +1,6 @@
-import { deleteSessionTokenCookie } from '$lib/server/session.js';
-import { sessionRepository } from '$lib/server/index.js';
-import { sha256 } from '@oslojs/crypto/sha2';
-import { encodeHexLowerCase } from '@oslojs/encoding';
+import { sessionService } from '$lib/server/session.js';
 
 export async function POST({ cookies }) {
-	const sessionToken = cookies.get('session');
-	if (sessionToken) {
-		const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(sessionToken)));
-		await sessionRepository.deleteSessionById(sessionId);
-		deleteSessionTokenCookie({ cookies });
-	}
+	await sessionService.DeleteSession({ cookies });
 	return new Response(null, { status: 204 });
 }
