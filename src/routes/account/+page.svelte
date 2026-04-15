2ded2078b13dedaf5e2b1fea0da81a9514d1910c
Joost <35542@ma-web.nl>
2026-04-15 11:08:35 +0200

    feat: setup, restored signout function and login username visible on page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -3,6 +3,8 @@
 	import walking from '$lib/assets/walking_together.svg';
 	import knowledge from '$lib/assets/sharing_knowledge.svg';
 	import NavButton from '$lib/components/NavButton.svelte';
+	let { data } = $props();
+	console.log(data);
 
 	let successMessage = $state('');
 	let errorMessage = $state('');
@@ -22,6 +24,40 @@
 		};
 		return handleSubmit;
 	}
+
+	async function handleSignOut(event) {
+		event.preventDefault();
+		await fetch('/logout', { method: 'POST' });
+		window.location.href = '/login';
+	}
 </script>
 
-<h1>Your Account</h1>
+<div class="account-card">
+	<h1>Username: {data.user.username}</h1>
+
+	<button on:click={handleSignOut} class="signout-btn">Sign out</button>
+</div>
+
+<style>
+	.signout-btn {
+		background-color: #b5006c;
+		color: var(--c-white);
+		border: 2px solid #b5006c;
+		border-radius: 20px;
+		width: 8rem;
+		height: 2.2rem;
+		font-size: 1rem;
+		font-weight: 600;
+		cursor: pointer;
+		transition:
+			background 0.2s,
+			color 0.2s,
+			border 0.2s;
+	}
+
+	.signout-btn:hover {
+		background-color: transparent;
+		color: #b5006c;
+		border: 2px solid #b5006c;
+	}
+</style>
