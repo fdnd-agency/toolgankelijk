3b9aad057f3ec34f55975824176978a553789905
JoostVDL4 <jvander.laan@ziggo.nl>
2026-04-16 15:21:55 +0200

    style: uses global.css values now

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,97 +1,98 @@
 <script>
-	import AccountIcon from '$lib/components/icons/accountIcon.svelte';
-	let { data } = $props();
+    import AccountIcon from '$lib/components/icons/accountIcon.svelte';
+    let { data } = $props();
 
-	async function handleSignOut(event) {
-		event.preventDefault();
-		await fetch('/logout', { method: 'POST' });
-		window.location.href = '/login';
-	}
+    async function handleSignOut(event) {
+        event.preventDefault();
+        await fetch('/logout', { method: 'POST' });
+        window.location.href = '/login';
+    }
 </script>
 
 <div class="account-card color-primary">
-	<div class="logo-box">
-		<span class="logo-text">
-			<AccountIcon class="btn-icon"></AccountIcon>
-		</span>
-	</div>
+    <div class="logo-box">
+        <span class="logo-text">
+            <AccountIcon class="btn-icon"></AccountIcon>
+        </span>
+    </div>
 
-	<h2>{data.user.username}</h2>
-	<h2 class="user-title">{data.user.email}</h2>
-	<button on:click={handleSignOut} class="signout-btn"> Sign out </button>
+    <h2>{data.user.username}</h2>
+    <h2 class="user-title">{data.user.email}</h2>
+    <button on:click={handleSignOut} class="signout-btn"> Sign out </button>
 </div>
 
 <style>
-	.account-card {
-		background-color: var(--light-2);
-		border-radius: 8px;
-		padding: 40px 124px;
-		margin: 10% auto 0 auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		width: 70%;
-		box-sizing: border-box;
-	}
+    .account-card {
+        background-color: var(--light-2);
+        border-radius: var(--border-radius);
+        padding: 40px 124px;
+        margin: 10% auto 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 70%;
+        box-sizing: border-box;
+    }
 
-	.logo-box {
-		background-color: white;
-		width: 12vh;
-		height: 12vh;
-		border-radius: 12px;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-bottom: 20px;
-	}
+    .logo-box {
+        background-color: var(--color-neutral-white);
+        width: 12vh;
+        height: 12vh;
+        border-radius: var(--border-radius);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
 
-	h2 {
-		margin: 0 0 24px 0;
-	}
+    h2 {
+        margin: 0 0 24px 0;
+    }
 
-	.logo-text {
-		font-size: 1.5rem;
-		font-weight: 800;
-		display: flex;
-		align-items: center;
-	}
+    .logo-text {
+        font-size: 1.5rem;
+        font-weight: 800;
+        display: flex;
+        align-items: center;
+    }
 
-	.user-title {
-		max-width: 100%;
-		white-space: nowrap;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
+    .user-title {
+        max-width: 100%;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
 
-	.signout-btn {
-		background-color: #f1d5e3;
-		border-radius: 24px;
-		padding: 8px 32px;
-		font-size: 1rem;
-		font-weight: 700;
-		cursor: pointer;
-		display: flex;
-		align-items: center;
-		gap: 8px;
-		transition: opacity 0.2s;
-		border: 2px solid black;
-		color: black;
-	}
+    .signout-btn {
+        background-color: var(--color-primary-light);
+        border-radius: 24px; 
+        padding: 8px 32px;
+        font-size: 1rem;
+        font-weight: 700;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        transition: opacity 0.2s;
+        border: 2px solid var(--color-neutral-black); 
+        color: var(--color-neutral-black); 
+    }
 
-	.signout-btn:hover {
-		background-color: transparent;
-		color: #b5006c;
-		opacity: 0.9;
-	}
-	.logo-text :global(svg) {
-		width: 50px;
-		height: 50px;
-		color: black;
-	}
+    .signout-btn:hover {
+        background-color: transparent;
+        color: var(--color-primary); 
+        opacity: 0.9;
+    }
+    
+    .logo-text :global(svg) {
+        width: 50px;
+        height: 50px;
+        color: var(--color-neutral-black); 
+    }
 
-	.logo-text :global(.btn-icon) {
-		width: 50px;
-		height: 50px;
-		color: #000000;
-	}
+    .logo-text :global(.btn-icon) {
+        width: 50px;
+        height: 50px;
+        color: var(--color-neutral-black);
+    }
 </style>
