97630f376f52053828a4894e8cc5a5c40038e20b
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-04-17 09:42:14 +0200

    build: input component
    this will work for all the inputs inside the webapplication

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -21,20 +21,10 @@
 	let {
 		iconName,
 		showIcon = false,
-		type,
-		variant = 'primary',
-		size = 'medium',
-		href,
-		as,
-		children,
-		onclick,
-		aria,
-		effect = null,
-		active = null,
-		...rest
+		addFor,
+		placeholderProp,
+		size
 	} = $props();
-
-	const tagname = $derived(as || (href ? 'a' : 'button'));
 
 	const iconMap = {
 		account: AccountIcon,
@@ -60,125 +50,38 @@
 	const ActiveIcon = $derived(iconName ? iconMap[iconName] : null);
 </script>
 
-<svelte:element
-	this={tagname}
-	{href}
-	{type}
-	class="navbutton {size} {variant} {active} {effect}"
-	{onclick}
-	{...rest}
-	aria-label={aria}
->
-	{#if showIcon && ActiveIcon}
+	<label for={addFor}>
+		{#if showIcon && ActiveIcon}
 		<span class="icon-wrapper">
 			<svelte:component this={ActiveIcon} />
 		</span>
 	{/if}
-	{@render children?.()}
-</svelte:element>
-
+	</label>
+	<input
+		{iconName}
+		placeholder={placeholderProp}
+		class="input {size}"
+	/>
 <style>
-	.navbutton {
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-		border-radius: var(--border-radius);
-		transition: all 0.2s ease;
-		cursor: pointer;
-		text-decoration: none;
-		border: var(--border-white, 1px solid transparent);
-		height: 3em;
-		gap: 0.5em;
-	}
 
-	.navbutton:hover {
-		filter: brightness(1.1);
-	}
+.full {
+	width: 100%;
+}
 
-	.navbutton:focus {
-		border: white 3px solid;
-	}
+input[type='text'] {
+	height: 48px;
+	width: 100%;
+	background-color: var(--color-neutral-white);
+	color: var(--color-neutral-black);
+	border: var(--color-neutral-black) 1px solid;
+	border-radius: 12px;
+	font-size: 16px;
+	padding-left: 1em;
 
-	.icon-wrapper {
-		display: inline-flex;
-		align-items: center;
+	@media (max-width: 720px) {
+			height: 32px;
+			font-size: 12px;
 	}
-
-	.small {
-		width: 3em;
-		padding: 1em 1em;
-		font-size: 16px;
-	}
-	.medium {
-		width: 8em;
-		padding: 1em 1em;
-		font-size: 16px;
-
-		@media (max-width: 1080px) {
-			width: 6em;
-		}
-	}
-	.large {
-		width: 12em;
-		padding: 1em 1em;
-		font-size: 16px;
-		justify-content: space-around;
-
-		@media (max-width: 1080px) {
-			width: 8em;
-		}
-	}
-	.xlarge {
-		width: 16em;
-		padding: 1em 1em;
-		font-size: 16px;
-		justify-content: space-around;
-	}
-
-	.primary {
-		background-color: var(--color-primary);
-		color: var(--color-neutral-white);
-		border: var(--color-neutral-lightgrey) 2px solid;
-	}
-
-	.secondary {
-		background-color: var(--color-primary-light);
-		color: var(--color-neutral-black);
-		border: var(--color-neutral-darkgrey) 2px solid;
-	}
-
-	.active {
-		background-color: var(--color-neutral-white);
-		color: var(--color-primary);
-	}
-
-	/* for the dropdowns */
-	.select {
-		width: 100%;
-		justify-content: space-between;
-		align-items: center;
-		border: var(--color-neutral-white) 1px solid;
-
-		&:focus {
-			transition-duration: 0.2s;
-			border: var(--color-neutral-white) 2px solid;
-		}
-	}
-
-	.dropdown {
-		width: 15em;
-		place-items: center;
-		margin: 0px auto;
-		justify-content: space-around;
-
-		@media (max-width: 720px) {
-			width: 100%;
-			justify-content: center;
-			gap: 1em;
-		}
-	}
-
-	.full {
-		width: 100%;
-	}
+}
+	
 </style>
