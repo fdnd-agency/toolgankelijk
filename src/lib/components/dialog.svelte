11f28a497e05e08025f37050832c979ae08fbce9
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-04-17 09:41:53 +0200

    style: removed the icons unnnessary for dialog
    #230

--- src/lib/components/dialog.svelte
+++ src/lib/components/dialog.svelte
@@ -1,23 +1,10 @@
 <script>
 	import Loader from '$lib/components/loader.svelte';
 	import NavButton from '$lib/components/NavButton.svelte';
-	import AccountIcon from '$lib/components/icons/accountIcon.svelte';
-	import AddIcon from '$lib/components/icons/addIcon.svelte';
-	import AlertIcon from '$lib/components/icons/alertIcon.svelte';
-	import AlphabeticalIcon from '$lib/components/icons/alphabeticalIcon.svelte';
 	import AuditIcon from '$lib/components/icons/auditIcon.svelte';
-	import BulbIcon from '$lib/components/icons/bulbIcon.svelte';
-	import CheckIcon from '$lib/components/icons/checkIcon.svelte';
-	import CrossIcon from '$lib/components/icons/crossIcon.svelte';
 	import DeleteIcon from '$lib/components/icons/deleteIcon.svelte';
 	import EditIcon from '$lib/components/icons/editIcon.svelte';
-	import FilterIcon from '$lib/components/icons/filterIcon.svelte';
-	import HomeIcon from '$lib/components/icons/homeIcon.svelte';
-	import InfoIcon from '$lib/components/icons/infoIcon.svelte';
-	import MenuIcon from '$lib/components/icons/menuIcon.svelte';
-	import MoonIcon from '$lib/components/icons/moonIcon.svelte';
-	import SearchIcon from '$lib/components/icons/searchIcon.svelte';
-	import SunIcon from '$lib/components/icons/sunIcon.svelte';
+
 
 	export let params;
 	export let isType;
