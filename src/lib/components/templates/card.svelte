790604cfaf1288f4479ae78f0ac9a6925e7faaaa
JoostVDL4 <jvander.laan@ziggo.nl>
2026-05-07 12:12:57 +0200

    feat: applied card background on more

--- src/lib/components/templates/card.svelte
+++ src/lib/components/templates/card.svelte
@@ -254,7 +254,7 @@
 	}
 
 	#card-url {
-		background-color: var(--color-neutral-grey);
+		background-color: var(--color-background-card);
 		padding: 1em;
 		border-radius: var(--border-radius);
 		width: 100%;
