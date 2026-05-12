5dcd80aafe83b17007205277382b81b4171eb642
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-05-12 14:24:40 +0200

    build: derived data
    the data will show up on every page
    
    #272

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,28 @@
 <script>
+	import { page } from '$app/stores';
 	import { onNavigate } from '$app/navigation';
 	import Header from '$lib/components/templates/header.svelte';
 	import SubHeader from '$lib/components/templates/subheader.svelte';
 
-	let { children } = $props();
+	let { data, children } = $props();
+
+	let params = $derived($page.params);
+
+	let websitesArray = $derived(
+		data.urlData?.url?.website?.urls || 
+		data.websitesData?.website?.urls || 
+		[]
+	);
+
+	let websites = $derived(data.websitesData || []);
+	let principles = $derived(data.principlesData?.principles || []);
+	let partners = $derived(data.partnersData || []);
+
+	let heading = $derived({
+		title: data.urlData?.url?.website?.title ?? data.websitesData?.website?.title ?? '',
+		homepage: data.urlData?.url?.url ?? '',
+		url: data.urlData?.url?.slug ?? ''
+	});
 
 	onNavigate((navigation) => {
 		if (!document.startViewTransition) return;
