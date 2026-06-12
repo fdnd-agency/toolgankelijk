f8d5b719bc7c110bcae56445738b876c06319c1e
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-06-12 14:16:31 +0200

    refactor: subheader
    the subheader is refactored according to the new design
    
    #273

--- src/lib/components/templates/subheader.svelte
+++ src/lib/components/templates/subheader.svelte
@@ -1,146 +1,174 @@
 <script>
-	import { page } from '$app/stores';
-	import NavButton from '$lib/components/molecules/navButton.svelte';
-	import BreadCrumbs from '$lib/components/organisms/breadCrumbs.svelte';
-	import Heading from '$lib/components/molecules/heading.svelte';
-	import Search from '$lib/components/molecules/search.svelte';
+    import { page } from '$app/stores';
+    import NavButton from '$lib/components/molecules/navButton.svelte';
+    import BreadCrumbs from '$lib/components/organisms/breadCrumbs.svelte';
+    import Heading from '$lib/components/molecules/heading.svelte';
+    import Search from '$lib/components/molecules/search.svelte';
 
-	let {
-		params,
-		user,
-		partners = [],
-		websites = [],
-		principles = [],
-		overview,
-		showAdd = false,
-		onAdd,
-		heading
-	} = $props();
+    let {
+        params,
+        user,
+        partners = [],
+        websites = [],
+        principles = [],
+        overview,
+        showAdd = false,
+        onAdd,
+        onApply,
+        heading
+    } = $props();
 
-	let principle = $state('All');
-	let level = $state('All');
-	let showNotMet = $state(false);
-	let showMet = $state(false);
+    let principle = $state('All');
+    let level = $state('All');
+    let showNotMet = $state(false);
+    let showMet = $state(false);
 
-	function handleSubmit(e) {
-		if (e) e.preventDefault();
+    function handleSubmit(e) {
+        if (e) e.preventDefault();
 
-		if (onApply) {
-			onApply({
-				principle,
-				level,
-				showNotMet,
-				showMet
-			});
-		}
-	}
+        if (onApply) {
+            onApply({
+                principle,
+                level,
+                showNotMet,
+                showMet
+            });
+        }
+    }
 </script>
 
 <div class="subheader">
-	<div class="subheader-heading">
-		<Heading {heading} />
-		{#if user && user.isEmailVerified}
-			<BreadCrumbs {params} {partners} {websites} {overview} {principles} width="full" />
-		{/if}
-	</div>
+    <!-- Top Row: Heading and Search/Add Actions -->
+    <div class="subheader-row top-row">
+        <div class="subheader-heading">
+            <Heading {heading} />
+        </div>
 
-	<div class="subheader-form">
-		<!-- this will be placed on a sidebar of the principles page -->
-		<div class="subheader-form-up-wrapper">
-			<NavButton size="small" variant="primary" showIcon={true} iconName="add" onclick={onAdd}
-			></NavButton>
-			<Search effect="disabled" />
-		</div>
+        <div class="subheader-actions">
+            <NavButton 
+                size="small" 
+                variant="primary" 
+                showIcon={true} 
+                iconName="add" 
+                effect="add"
+                onclick={onAdd}
+                aria="Add Partner"
+            />
+            
+            <div class="search-wrapper">
+                <input class="search-tool-subheader" type="text" placeholder="Search..."> 
+                <!-- If you are using the <Search /> component instead, place it here -->
+            </div>
+        </div>
+    </div>
 
-		<div class="subheader-form-down-wrapper">
-			<p>Sorteren op</p>
+    <!-- Bottom Row: Breadcrumbs and Sorting -->
+    <div class="subheader-row bottom-row">
+        <div class="subheader-breadcrumbs">
+            {#if user && user.isEmailVerified}
+                <BreadCrumbs {params} {partners} {websites} {overview} {principles} />
+            {/if}
+        </div>
 
-			<NavButton effect="disabled">Levels</NavButton>
-
-			<NavButton effect="disabled">Principles</NavButton>
-		</div>
-
-		<div class="subheader-dropdown-wrapper">
-			<NavButton
-				size="small"
-				showIcon={true}
-				iconName="arrow"
-				effect="disabled"
-				aria="dropdown menu"
-			></NavButton>
-		</div>
-	</div>
+        <div class="subheader-filters">
+            <p class="filter-label">Sorteren op:</p>
+            <NavButton effect="dropdown" size="medium" showIcon={true} iconName="arrow">Levels</NavButton>
+            <NavButton effect="dropdown" size="medium" showIcon={true} iconName="arrow">Principles</NavButton>
+        </div>
+    </div>
 </div>
 
 <style>
-	.subheader {
-		height: 10em;
-		padding-left: 5%;
-		padding-right: 5%;
-		align-items: center;
-		display: flex;
-		justify-content: space-between;
-		width: 100vw;
+    .subheader {
+        display: flex;
+        flex-direction: column;
+        gap: 1.5em;
+        width: 90%;
+        margin-top: 1em;
+        margin-bottom: 2em;
+		margin-left: 5%;
+		margin-right: 5%;
+    }
 
-		@media (max-width: 1080px) {
-			margin-top: 1em;
-			margin-bottom: 1em;
-		}
+    .subheader-row {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        gap: 1em;
+    }
 
-		@media (max-width: 720px) {
-			display: flex;
-			gap: 0.5em;
-			padding-right: 5em;
-		}
-	}
+    .subheader-heading {
+        display: flex;
+        align-items: center;
+        flex-shrink: 0;
+    }
 
-	.subheader-heading {
-		display: flex;
-		margin-left: 2%;
-		flex-direction: column;
-		gap: 0.5em;
-		width: 100%;
-	}
+    .subheader-actions {
+        display: flex;
+        gap: 1em;
+        align-items: center;
+    }
 
-	.subheader-form {
-		display: flex;
-		gap: 0.5em;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
+    .subheader-breadcrumbs {
+        display: flex;
+        align-items: center;
+        flex-grow: 1;
+    }
 
-	.subheader-form-up-wrapper {
-		display: flex;
-		justify-content: flex-end;
-		align-items: center;
+    .subheader-filters {
+        display: flex;
+        gap: 1em;
+        align-items: center;
+    }
 
-		@media (max-width: 1080px) {
-			padding-left: 4em;
-		}
+    .filter-label {
+        font-size: 0.9em;
+        margin: 0;
+        color: var(--color-neutral-black, #000);
+        white-space: nowrap;
+    }
 
-		@media (max-width: 1080px) {
-			display: none;
-		}
-	}
+    .search-wrapper {
+        display: flex;
+        align-items: center;
+    }
 
-	.subheader-form-down-wrapper {
-		display: flex;
-		gap: 1em;
-		align-items: center;
-		justify-content: flex-start;
+    .search-tool-subheader {
+        height: 3em;
+        border-radius: 1.5em; /* Creates the pill-shape from the design */
+        border: 2px solid var(--color-neutral-black, #000);
+        padding: 0 1em;
+        font-size: 1em;
+        min-width: 200px;
+        outline: none;
+        transition: border-color 0.2s ease;
+    }
 
-		@media (max-width: 1080px) {
-			display: none;
-		}
-	}
+    .search-tool-subheader:focus {
+        border-color: var(--color-primary, #b30059);
+    }
 
-	.subheader-dropdown-wrapper {
-		padding-bottom: 1em;
-		margin-left: 1em;
+    /* Mobile Responsiveness */
+    @media (max-width: 1080px) {
+        .subheader {
+            gap: 1em;
+        }
 
-		@media (min-width: 1080px) {
-			display: none;
-		}
-	}
+        .subheader-filters {
+            display: none; /* Keep sorting hidden on smaller screens as in original code */
+        }
+    }
+
+    @media (max-width: 720px) {
+        .subheader-row {
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        
+        .subheader-actions {
+            width: 100%;
+            justify-content: space-between;
+        }
+    }
 </style>
