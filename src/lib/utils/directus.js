e536b26d6ba83ba3d9a9d059bfadb4461c975ea1
MaksimH2O <Maksim.hofker@protonmail.com>
2026-06-12 11:34:19 +0200

    refactor: changed VITE_DIRECTUS_KEY to DIRECTUS_STATIC_TOKEN

--- src/lib/utils/directus.js
+++ src/lib/utils/directus.js
@@ -1,12 +1,12 @@
 //@ts-check
-import { DIRECTUS_URL, VITE_DIRECTUS_KEY } from '$env/static/private';
+import { DIRECTUS_URL, DIRECTUS_STATIC_TOKEN } from '$env/static/private';
 import { createDirectus, graphql, rest, staticToken } from '@directus/sdk';
 
 const baseUrl = DIRECTUS_URL.replace(/\/+$/, '');
-const token = VITE_DIRECTUS_KEY;
+const token = DIRECTUS_STATIC_TOKEN;
 
 if (!baseUrl || !token) {
-	throw new Error('DIRECTUS_URL and VITE_DIRECTUS_KEY must be set');
+	throw new Error('DIRECTUS_URL and DIRECTUS_STATIC_TOKEN must be set');
 }
 
 export const directusClient = createDirectus(baseUrl)
